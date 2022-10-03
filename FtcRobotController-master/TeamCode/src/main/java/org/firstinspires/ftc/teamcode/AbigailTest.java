bc9d370da0103d2b3c13346b3bc755d0621a568c
“AbigailJoseph” <abigailmj1905@gmail.com>
2022-10-03 19:01:09 -0400

    teleop code

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AbigailTest.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AbigailTest.java
@@ -1,4 +1,96 @@
 package org.firstinspires.ftc.teamcode;
 
-public class AbigailTest {
-}
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name="testTeleopAbigail")
+@Disabled
+public class AbigailTest extends LinearOpMode{
+
+
+    //declare variables here
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor frontLeft = null;
+    private DcMotor backLeft = null;
+    private DcMotor frontRight = null;
+    private DcMotor backRight = null;
+
+    @Override
+    public void runOpMode() {
+
+        //mapping here
+        frontLeft  = hardwareMap.get(DcMotor.class, "frontLeft");
+        backLeft  = hardwareMap.get(DcMotor.class, "backLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+        frontRight.setDirection(DcMotor.Direction.FORWARD);
+        backRight.setDirection(DcMotor.Direction.FORWARD);
+
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset(); //resets the
+
+        while(opModeIsActive()){
+
+            /* TESTS IF SETDIRECTION CODE IS WORKING CORRECTLY
+            leftFrontPower  = gamepad1.x ? 1.0 : 0.0;  // X gamepad
+            leftBackPower   = gamepad1.a ? 1.0 : 0.0;  // A gamepad
+            rightFrontPower = gamepad1.y ? 1.0 : 0.0;  // Y gamepad
+            rightBackPower  = gamepad1.b ? 1.0 : 0.0;  // B gamepad
+            */
+
+            double max;
+
+            // Right joystick to go forward & strafe, and left joystick to rotate.
+            double axial   = gamepad1.right_stick_y;
+            double lateral =  -gamepad1.right_stick_x;
+            double yaw =  -gamepad1.left_stick_x; // Note: pushing stick forward gives NEG value
+
+            // This ensures that the robot maintains the desired motion.
+            double frontLeftPower  = axial + lateral + yaw;
+            double frontRightPower = axial - lateral - yaw;
+            double backLeftPower   = axial - lateral + yaw;
+            double backRightPower  = axial + lateral - yaw;
+
+
+            max = Math.max(Math.abs(frontLeftPower), Math.abs(frontRightPower));
+            max = Math.max(max, Math.abs(backLeftPower));
+            max = Math.max(max, Math.abs(backRightPower));
+
+            // Normalize the values so no wheel power exceeds 100%
+            if (max > 1.0) {
+                frontLeftPower  /= max;
+                frontRightPower /= max;
+                backLeftPower   /= max;
+                backRightPower  /= max;
+            }
+
+            // Send calculated power to wheels
+            frontLeft.setPower(frontLeftPower);
+            backLeft.setPower(frontLeftPower);
+            frontRight.setPower(backLeftPower);
+            backRight.setPower(backRightPower);
+
+            // Show the elapsed game time and wheel power.
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Front left/Right", "%4.2f, %4.2f", frontLeftPower, frontLeftPower);
+            telemetry.addData("Back  left/Right", "%4.2f, %4.2f", backLeftPower, backRightPower);
+            telemetry.update();
+
+            }
+
+        }
+
+
+    }
+
