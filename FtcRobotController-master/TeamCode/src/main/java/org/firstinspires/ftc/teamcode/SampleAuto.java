f0ba7c891c207259768713c7d5a83d0cae77242e
“AbigailJoseph” <abigailmj1905@gmail.com>
2022-10-19 17:54:05 -0400

    sample wheel auto

---
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleAuto.java
@@ -0,0 +1,83 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class SampleAuto extends LinearOpMode {
+
+    private DcMotor frontLeft = null;
+    private DcMotor backLeft = null;
+    private DcMotor frontRight = null;
+    private DcMotor backRight = null;
+    private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode(){
+        frontLeft  = hardwareMap.get(DcMotor.class, "frontLeft");
+        backLeft  = hardwareMap.get(DcMotor.class, "backLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+        frontRight.setDirection(DcMotor.Direction.FORWARD);
+        backRight.setDirection(DcMotor.Direction.FORWARD);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+        runtime.reset();
+
+        // Drive forward for 3 seconds
+        frontLeft.setPower(1.0);
+        backLeft.setPower(1.0);
+        frontRight.setPower(1.0);
+        backRight.setPower(1.0);
+
+        runtime.reset();
+
+        while (opModeIsActive() && (runtime.seconds() < 3.0)) {
+            telemetry.addData("Path", "Leg 1: %4.1f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+
+        //Drive left for 5 seconds
+        frontLeft.setPower(-1.0);
+        backLeft.setPower(1.0);
+        frontRight.setPower(1.0);
+        backRight.setPower(-1.0);
+
+        runtime.reset();
+
+        while (opModeIsActive() && (runtime.seconds() < 5.0)) {
+            telemetry.addData("Path", "Leg 1: %4.1f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+
+        //Turn right for 2.2 seconds
+        frontLeft.setPower(1.0);
+        backLeft.setPower(1.0);
+        frontRight.setPower(-1.0);
+        backRight.setPower(-1.0);
+
+        runtime.reset();
+
+        while (opModeIsActive() && (runtime.seconds() < 2.2)) {
+            telemetry.addData("Path", "Leg 1: %4.1f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+
+        //Stop
+        frontLeft.setPower(0);
+        backLeft.setPower(0);
+        frontRight.setPower(0);
+        backRight.setPower(0);
+
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+        sleep(1000);
+    }
+
+}
