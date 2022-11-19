377ce2ba1c0941a36979e9641d2d0d2ed4db0868
“AbigailJoseph” <abigailmj1905@gmail.com>
2022-11-19 11:58:52 -0500

    diagonal teleop

---
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class Teleop extends LinearOpMode {
+
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor frontLeft = null;
+    private DcMotor backLeft = null;
+    private DcMotor frontRight = null;
+    private DcMotor backRight = null;
+
+    @Override
+    public void runOpMode(){
+        frontLeft  = hardwareMap.get(DcMotor.class, "frontLeft");
+        backLeft  = hardwareMap.get(DcMotor.class, "backLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+        frontRight.setDirection(DcMotor.Direction.FORWARD);
+        backRight.setDirection(DcMotor.Direction.FORWARD);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset(); //resets the
+
+        while(opModeIsActive()){
+
+            double speed;
+            double strafe;
+            double turn;
+
+            speed = -gamepad1.right_stick_y; //needs to be negative if right motors are flipped
+            strafe = gamepad1.right_stick_x;
+            turn = gamepad1.left_stick_x;
+
+            /*frontLeft.setPower(pivot + (vertical - horizontal));
+            frontRight.setPower(pivot + (vertical + horizontal));
+            backLeft.setPower(pivot + (vertical + horizontal));
+            backRight.setPower(pivot + (vertical - horizontal)); */
+
+            frontLeft.setPower(speed+turn+strafe);
+            frontRight.setPower(speed-turn-strafe);
+            backLeft.setPower(speed+turn-strafe);
+            backRight.setPower(speed-turn+strafe);
+        }
+    }
+
+}
