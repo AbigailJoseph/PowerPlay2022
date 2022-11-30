dd674fe79ec6e9f370ef7a30100059c56dcd6906
“AbigailJoseph” <abigailmj1905@gmail.com>
2022-11-30 16:50:54 -0500

    teleop shell

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -3,7 +3,11 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 @TeleOp(name="Teleop")
 public class Teleop extends LinearOpMode {
@@ -13,6 +17,13 @@ public class Teleop extends LinearOpMode {
     private DcMotor backLeft = null;
     private DcMotor frontRight = null;
     private DcMotor backRight = null;
+    private DcMotor arm = null;
+    private Servo claw = null;
+    private double clawPosition;
+
+    //FILL THESE IN
+    //static final double CLAWCLOSE = ;
+    //static final double CLAWOPEN = ;
 
     @Override
     public void runOpMode(){
@@ -35,6 +46,7 @@ public class Teleop extends LinearOpMode {
 
         while(opModeIsActive()){
 
+            //                                   [GAME PAD 1]
             double speed;
             double strafe;
             double turn;
@@ -43,15 +55,61 @@ public class Teleop extends LinearOpMode {
             strafe = gamepad1.right_stick_x;
             turn = gamepad1.left_stick_x;
 
-            /*frontLeft.setPower(pivot + (vertical - horizontal));
-            frontRight.setPower(pivot + (vertical + horizontal));
-            backLeft.setPower(pivot + (vertical + horizontal));
-            backRight.setPower(pivot + (vertical - horizontal)); */
-
+            // WHEEL MOVEMENT(left stick) & ROTATION(right stick)
             frontLeft.setPower(speed+turn+strafe);
             frontRight.setPower(speed-turn-strafe);
             backLeft.setPower(speed+turn-strafe);
             backRight.setPower(speed-turn+strafe);
+
+            //                                  [GAME PAD 2]
+
+            // ARM TEST(button x & y)
+            if(gamepad2.x){
+                arm.setPower(0.3);
+            }
+            else if(gamepad2.y){
+                arm.setPower(0.3);;
+            }
+            else{
+                arm.setPower(0);
+            }
+
+            // ARM(right stick)
+            /*
+            if(gamepad2.right_stick_y > ){ //moves up or down
+                arm.setPower();
+            }
+            else if(gamepad2.right_stick_y < ){ //moves up or down
+                arm.setPower();;
+            }
+            else{
+                arm.setPower(0);
+            }
+            */
+
+            // CLAW TEST(button a & b)
+            if(gamepad2.a){ //open or close
+                clawPosition += 0.01;
+            }
+            else if(gamepad2.b){ //open or close
+                clawPosition -= 0.01;
+            }
+            clawPosition = Range.clip(clawPosition, -1.0, 1.0);
+            claw.setPosition(clawPosition);
+            telemetry.addData("Servo Position: ", clawPosition);
+            telemetry.update();
+
+            //CLAW(button a & b)
+            /*
+            if(gamepad2.a){ //open or close
+                clawPosition = CLAWCLOSE;
+            }
+            else if(gamepad2.b){ //open or close
+                clawPosition = CLAWOPEN;
+            }
+            clawPosition = Range.clip(clawPosition, -1.0, 1.0);
+            claw.setPosition(clawPosition);
+             */
         }
     }
 
